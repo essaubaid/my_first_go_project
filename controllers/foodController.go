b062ecc34dbb15012e0ca6996bfd24f000f3f9f1
essaubaid <essaubaid0@gmail.com>
2024-04-06 19:50:02 +0500

    Foods controller completed

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"context"
 	"log"
+	"math"
 	"net/http"
 	"strconv"
 	"time"
@@ -11,8 +12,19 @@ import (
 	"github.com/essaubaid/my_first_go_project/models"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+func round(num float64) int {
+	return int(num + math.Copysign(0.5, num))
+}
+
+func toFixed(num float64, precision int) float64 {
+	output := math.Pow(10, float64(precision))
+	return float64(round(num*output)) / output
+}
 
 var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "menu")
 
@@ -117,5 +129,123 @@ func GetFood() gin.HandlerFunc {
 		}
 
 		c.JSON(http.StatusOK, food)
+	}
+}
+
+func CreateFood() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var menu models.Menu
+		var food models.Food
+
+		if err := c.BindJSON(&food); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		if validationError := validate.Struct(food); validationError != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": validationError.Error(),
+			})
+			return
+		}
+
+		err := menuCollection.FindOne(ctx, bson.M{"menu_id": food.Menu_id}).Decode(&menu)
+
+		if err != nil {
+			msg := "menu was not found"
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": msg,
+			})
+			return
+		}
+
+		food.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		food.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		food.ID = primitive.NewObjectID()
+		food.Food_id = food.ID.Hex()
+		var num = toFixed(*food.Price, 2)
+		food.Price = &num
+
+		result, insertErr := foodCollection.InsertOne(ctx, food)
+		if insertErr != nil {
+			msg := "Food item was not created"
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": msg,
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, result)
+	}
+
+}
+
+func UpdateFood() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		var menu models.Menu
+		var food models.Food
+
+		foodId := c.Param("food_id")
+		if err := c.BindJSON(&food); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		updateObj := bson.M{}
+
+		if food.Name != nil {
+			updateObj["name"] = food.Name
+		}
+		if food.Price != nil {
+			updateObj["price"] = food.Price
+		}
+		if food.Food_image != nil {
+			updateObj["food_image"] = food.Food_image
+		}
+		if food.Menu_id != nil {
+			err := menuCollection.FindOne(ctx, bson.M{"menu_id": food.Menu_id}).Decode(&menu)
+			if err != nil {
+				msg := "Menu was not found"
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"error": msg,
+				})
+				return
+			}
+			updateObj["menu_id"] = food.Menu_id
+		}
+		food.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		updateObj["updated_at"] = food.Updated_at
+
+		upsert := true
+		filter := bson.M{"food_id": foodId}
+
+		opt := options.UpdateOptions{
+			Upsert: &upsert,
+		}
+
+		result, err := foodCollection.UpdateOne(
+			ctx,
+			filter,
+			bson.D{
+				{Key: "$set", Value: updateObj},
+			},
+			&opt,
+		)
+
+		if err != nil {
+			msg := "foot item update failed"
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+		c.JSON(http.StatusOK, result)
 	}
 }
--- routes/foodRouter.go
+++ routes/foodRouter.go
@@ -1,10 +1,13 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/essaubaid/my_first_go_project/controllers"
+	"github.com/gin-gonic/gin"
+)
 
 func FoodRouter(incomingRoutes *gin.Engine) {
-	incomingRoutes.GET("/foods")
-	incomingRoutes.GET("/food/:food_id")
-	incomingRoutes.POST("/foods")
-	incomingRoutes.PATCH("/foods/:food_id")
+	incomingRoutes.GET("/foods", controllers.GetFoods())
+	incomingRoutes.GET("/food/:food_id", controllers.GetFood())
+	incomingRoutes.POST("/food", controllers.CreateFood())
+	incomingRoutes.PATCH("/food/:food_id", controllers.UpdateFood())
 }
