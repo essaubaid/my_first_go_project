4c8f51784c4a550d700d00e299aaf5e1783f8288
essaubaid <essaubaid0@gmail.com>
2024-04-08 22:58:10 +0500

    Create invoices created

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -10,6 +10,7 @@ import (
 	"github.com/essaubaid/my_first_go_project/models"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -85,5 +86,62 @@ func GetInvoice() gin.HandlerFunc {
 		invoiceView.Order_details = allOrderItems[0]["order_items"]
 
 		c.JSON(http.StatusOK, invoiceView)
+	}
+}
+
+func CreateInvoice() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		var invoice models.Invoice
+
+		if err := c.BindJSON(invoice); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		var order models.Order
+
+		if err := orderCollection.FindOne(ctx, bson.M{"order_id": invoice.Order_id}).Decode(&order); err != nil {
+			msg := "Order was not found"
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": msg,
+			})
+			return
+		}
+
+		status := "PENDING"
+
+		if invoice.Payment_status == nil {
+			invoice.Payment_status = &status
+		}
+
+		invoice.Payment_due_date, _ = time.Parse(time.RFC3339, time.Now().AddDate(0, 0, 1).Format(time.RFC3339))
+		invoice.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		invoice.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		invoice.ID = primitive.NewObjectID()
+		invoice.Invoice_id = invoice.ID.Hex()
+
+		validationErr := validate.Struct(invoice)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": validationErr.Error(),
+			})
+			return
+		}
+
+		result, insertErr := invoiceCollection.InsertOne(ctx, invoice)
+
+		if insertErr != nil {
+			msg := "invoice item was not created"
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": msg,
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, result)
 	}
 }
