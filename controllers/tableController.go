03bbe6249d26cc2656d49aeef8aaa59e7a114907
essaubaid <essaubaid0@gmail.com>
2024-04-09 00:06:43 +0500

    get table complete

--- controllers/tableController.go
+++ controllers/tableController.go
@@ -1,8 +1,34 @@
 package controllers
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/essaubaid/my_first_go_project/database"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var tableCollection *mongo.Collection = database.OpenCollection(database.Client, "table")
+
+func GetTables() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		result, err := tableCollection.Find(ctx, bson.M{})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "error occurred while listing the tables",
+			})
+			return
+		}
+		var allTables []bson.M
+		if err = result.All(ctx, &allTables); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allTables)
+	}
+}
