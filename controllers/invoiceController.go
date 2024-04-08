00f1e689e3d6d229de06a0a0e9c4200089257b78
essaubaid <essaubaid0@gmail.com>
2024-04-08 22:42:40 +0500

    Get Invoices complete

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -61,9 +61,29 @@ func GetInvoice() gin.HandlerFunc {
 			return
 		}
 
-		// var invoiceView InvoiceViewFormat
+		var invoiceView InvoiceViewFormat
 
-		// allOrderItems, err:= ItemsByOrder(invoice.Order_id)
+		allOrderItems, err := ItemsByOrder(invoice.Order_id)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "error while fetching the order details for this invoice",
+			})
+			return
+		}
+		invoiceView.Order_id = invoice.Order_id
+		invoiceView.Payment_due_date = invoice.Payment_due_date
 
+		invoiceView.Payment_method = "null"
+		if invoice.Payment_method != nil {
+			invoiceView.Payment_method = *invoice.Payment_method
+		}
+
+		invoiceView.Invoice_id = invoice.Invoice_id
+		invoiceView.Payment_status = invoice.Payment_status
+		invoiceView.Payment_due = allOrderItems[0]["payment_due"]
+		invoiceView.Table_number = allOrderItems[0]["table_number"]
+		invoiceView.Order_details = allOrderItems[0]["order_items"]
+
+		c.JSON(http.StatusOK, invoiceView)
 	}
 }
