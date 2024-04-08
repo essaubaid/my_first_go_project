507c0227e45c1e570e2b4b844e353cc01e5d844f
essaubaid <essaubaid0@gmail.com>
2024-04-08 23:34:32 +0500

    update Invoice complete

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -12,6 +12,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type InvoiceViewFormat struct {
@@ -139,6 +140,63 @@ func CreateInvoice() gin.HandlerFunc {
 			c.JSON(http.StatusInternalServerError, gin.H{
 				"error": msg,
 			})
+			return
+		}
+
+		c.JSON(http.StatusOK, result)
+	}
+}
+
+func UpdateInvoice() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var invoice models.Invoice
+		invoiceId := c.Param("invoice_id")
+
+		if err := c.BindJSON(invoice); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		filter := bson.M{
+			"invoice_id": invoiceId,
+		}
+
+		updateObj := bson.M{}
+
+		if invoice.Payment_method != nil {
+			updateObj["payment_method"] = invoice.Payment_method
+		}
+		if invoice.Payment_status != nil {
+			updateObj["payment_status"] = invoice.Payment_status
+		}
+		invoice.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		updateObj["updated_at"] = invoice.Updated_at
+
+		upsert := true
+		opt := options.UpdateOptions{
+			Upsert: &upsert,
+		}
+
+		status := "PENDING"
+		if invoice.Payment_status == nil {
+			invoice.Payment_status = &status
+		}
+
+		result, err := invoiceCollection.UpdateOne(
+			ctx, filter, bson.D{
+				{Key: "$set", Value: updateObj},
+			},
+			&opt,
+		)
+
+		if err != nil {
+			msg := "invoice item update failed"
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
 			return
 		}
 
