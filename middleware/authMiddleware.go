49e248700ef7378cbc233a5ee2069bdefb3368c9
essaubaid <essaubaid0@gmail.com>
2024-04-11 00:26:38 +0500

    Auth middleware complete

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -1,1 +1,38 @@
 package middleware
+
+import (
+	"net/http"
+
+	"github.com/essaubaid/my_first_go_project/helpers"
+	"github.com/gin-gonic/gin"
+)
+
+func Authentication() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		clientToken := c.Request.Header.Get("token")
+		if clientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "No Authorization header provided",
+			})
+			c.Abort()
+			return
+		}
+
+		claim, err := helpers.ValidateToken(clientToken)
+
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err,
+			})
+			c.Abort()
+			return
+		}
+
+		c.Set("email", claim.Email)
+		c.Set("first_name", claim.First_name)
+		c.Set("last_name", claim.Last_name)
+		c.Set("uid", claim.Uid)
+
+		c.Next()
+	}
+}
