af5f6277fdf093b12d4e8c970e1cd13111d45349
essaubaid <essaubaid0@gmail.com>
2024-04-11 00:16:46 +0500

    token helper complete

--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -1,11 +1,16 @@
 package helpers
 
 import (
+	"context"
 	"log"
 	"os"
 	"time"
 
+	"github.com/essaubaid/my_first_go_project/database"
 	"github.com/golang-jwt/jwt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type SignedDetails struct {
@@ -16,6 +21,7 @@ type SignedDetails struct {
 	jwt.StandardClaims
 }
 
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
 var secret_key string = os.Getenv("SECRET_KEY")
 
 func GenerateAllTokens(email string, firstName string, lastName string, uid string) (signedToken string, signedRefreshToken string, err error) {
@@ -44,4 +50,63 @@ func GenerateAllTokens(email string, firstName string, lastName string, uid stri
 
 	return token, refreshToken, err
 
+}
+
+func UpdateAllTokens(signedToken string, signedRefreshToken string, userId string) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	updateObj := bson.M{}
+
+	updateObj["token"] = signedToken
+	updateObj["refresh_token"] = signedRefreshToken
+
+	Updated_at, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	updateObj["updated_at"] = Updated_at
+
+	upsert := true
+	filter := bson.M{"user_id": userId}
+
+	opt := options.UpdateOptions{
+		Upsert: &upsert,
+	}
+
+	_, err := userCollection.UpdateOne(
+		ctx,
+		filter,
+		bson.D{
+			{Key: "$set", Value: updateObj},
+		},
+		&opt,
+	)
+
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+}
+
+func ValidateToken(signedToken string) (claims *SignedDetails, msg string) {
+	token, err := jwt.ParseWithClaims(
+		signedToken,
+		&SignedDetails{},
+		func(t *jwt.Token) (interface{}, error) { return []byte(secret_key), nil },
+	)
+
+	claims, ok := token.Claims.(*SignedDetails)
+
+	if !ok {
+		// the token is invalid
+		msg = err.Error()
+		return
+	}
+
+	if claims.ExpiresAt < time.Now().Local().Unix() {
+		// token is expired
+		msg = err.Error()
+		return
+	}
+
+	return claims, msg
 }
